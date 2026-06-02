d3ac3cb6bee602a842ac7369f83bcc584f271cc0
fernando <ferdnobre@gmail.com>
2026-06-02 17:43:51 -0300

    pri

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { Elysia } from "elysia";
 
 const app = new Elysia().get("/", () => "Hello Elysia").listen(3000);
+const even = process.env.nome
 
 console.log(
-  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}`
+  `🦊 Elysia is running at ${app.server?.hostname}:${app.server?.port}
+  nome = ${even}
+  `
 );
+
